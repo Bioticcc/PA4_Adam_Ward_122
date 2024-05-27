3fe3a11d11d9322d5ac7b0bd30152e6413b29b08
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-05-27 13:54:37 -1000

    just startin!
    got zeh libraries in!

---
+++ PA4_Adam_Ward_122/Header.h
@@ -0,0 +1,23 @@
+/*
+Author: Adam Ward
+Date: 5/27/2024
+Description: PA4, a athletic thing.
+*/
+
+
+#pragma once
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>   
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+
+
+#endif 
+
---
+++ PA4_Adam_Ward_122/main.cpp
@@ -0,0 +1,10 @@
+#include "Header.h"
+using namespace std;
+int main() {
+
+
+
+	return 0;
+
+
+}
