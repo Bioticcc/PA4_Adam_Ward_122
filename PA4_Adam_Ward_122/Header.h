efdc8f423e7bb1acade4f9052451d05e19232a89
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-05-29 13:43:02 -1000

    Got DietPlan class done (i think?)
    makin progress!

--- PA4_Adam_Ward_122/Header.h
+++ PA4_Adam_Ward_122/Header.h
@@ -22,17 +22,62 @@ using std::cout;
 using std::cin; 
 using std::string;
 
+
 //defining classes!
 class DietPlan {
-public:
+	//defining my classes variables
 	int goalCal;
 	string planName;
 	string planDate;
 
-	DietPlan(int x, string y, string z) {
+public:
+	
+
+	//the constructor. It should set up the initial values of the class.
+	DietPlan(int x, const string &y, const string &z) {
 		goalCal = x;
 		planName = y;
 		planDate = z;
+	}
+
+	//the copy constructor. copies? i think? its weird
+	DietPlan(const DietPlan& t) {
+		goalCal = t.goalCal;
+		planName = t.planName;
+		planDate = t.planDate;
+	}
+
+	//the deconstructor
+	~DietPlan() {
+
+	}
+
+	//getter functions! yahooie
+	//so these are bassically what let me print, and view the variables within my class
+	int getGoalCal() const {
+		return goalCal;
+	}
+
+	const string getPlanName() const {
+		return planName;
+	}
+
+	const string getPlanDate() const {
+		return planDate;
+	}
+
+	//setter functions! hooieya!
+	//these, as the name suggests, let me SET the variables to things!
+	void setGoalCal(int num) {
+		goalCal = num;
+	}
+
+	void setPlanName(const string str) {
+		planName = str;
+	}
+
+	void setPlanDate(const string str) {
+		planDate = str;
 	}
 };
 
--- PA4_Adam_Ward_122/main.cpp
+++ PA4_Adam_Ward_122/main.cpp
@@ -1,8 +1,44 @@
 #include "Header.h"
 int main() {
+
+	//initializing dietplan as a plan! yahoo
 	DietPlan plan(500, "planA", "2/12/2005");
-	cout << "test: " << plan.goalCal << "\ntest: " << plan.planDate << "\ntest: " << plan.planName;
-	return 0;	
 
+	//testing initial values
+	cout << "testing goalCal initial: " << plan.getGoalCal() << "\n";
+	cout << "testing planName initial: " << plan.getPlanName() << "\n";
+	cout << "testing planDate initial: " << plan.getPlanDate() << "\n\n";
 
+	//testing copy constructer values
+	DietPlan planC = plan;
+	cout << "testing goalCal initial copy: " << planC.getGoalCal() << "\n";
+	cout << "testing planName initial copy: " << planC.getPlanName() << "\n";
+	cout << "testing planDate initial copy: " << planC.getPlanDate() << "\n\n";
+
+	//testing editable values / inputs 
+	int num;
+	string str;
+	string str1;
+
+	cout << "enter goal calories: ";
+	cin >> num;
+	plan.setGoalCal(num);
+
+	cout << "enter plan name: ";
+	cin >> str;
+	plan.setPlanName(str);
+
+	cout << "enter plan date: ";
+	cin >> str1;
+	plan.setPlanDate(str1);
+
+	//printing edited values
+	cout << "\n";
+	cout << "testing goalCal edited: " << plan.getGoalCal() << "\n";
+	cout << "testing planName edited: " << plan.getPlanName() << "\n";
+	cout << "testing planDate edited: " << plan.getPlanDate() << "\n\n";
+
+	return 0;
 }
+
+	
